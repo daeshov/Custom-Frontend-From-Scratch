4906cbe923b6d644682c192366d2348be780d15d
daeshov <jdarahthomas@gmail.com>
2023-08-10 11:38:21 -0500

    task template setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 // import './index.css'
-import './styles.css'
+// import './styles.css'
 import 'semantic-ui-css/semantic.min.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
