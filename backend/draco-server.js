9d82dc6ddb7b8b76cdaccb77632fb6e86f83b821
evanrich2404 <evanrich2404@gmail.com>
2023-08-19 23:52:53 -0500

    gonna push this

--- backend/draco-server.js
+++ backend/draco-server.js
@@ -1,53 +1,22 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3001;
+const express = require('express')
+const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Dragon Bed API is up and running!');
+// Routes
+app.get('/api', (req, res) => {
+    res.json({ "message": "Welcome to Scales & Slumber!" });
 });
+
+// Server start
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
+// app.post('/api/generate', (req, res) => {
+//   // Code to generate a new design
+// });
 
-const mongoose = require('mongoose');
-
-// Replace this URL with your MongoDB connection string
-const MONGODB_URI = 'mongodb://localhost:5173/placeholder';
-
-mongoose.connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,  // Ensure you hash the password before storing!
-  // any other user details you wish to store
-});
-
-const productSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  image: String, // URL to the image
-  // any other product details
-});
-
-app.post('/api/generate', (req, res) => {
-  // Code to generate a new design
-});
-
-app.get('/api/designs', (req, res) => {
-  // Code to retrieve previous designs
-});
-
-
-const cors = require('cors');
-
-// Use CORS middleware
-app.use(cors());
-
+// app.get('/api/designs', (req, res) => {
+//   // Code to retrieve previous designs
+// });
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,15 +3,15 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  root: './frontend', // Set the project root to the frontend directory
+  root: '.', // Set the project root to the current directory
   
   server: {
-    publicDir: './frontend/public' // Adjust the public directory path
+    publicDir: './public' // Adjust the public directory path
   },
   
   resolve: {
     alias: {
-      '@': './frontend/src'
+      '@': './src'
     }
   }
 });
