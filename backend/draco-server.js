ecbb2d21f5efba39c29e45c2a6328b3c92c338ca
evanrich2404 <evanrich2404@gmail.com>
2023-08-23 14:25:53 -0500

    changed products.csv to no avail/image route added

--- backend/draco-server.js
+++ backend/draco-server.js
@@ -27,6 +27,10 @@ app.get('/api/products', async (req, res) => {
     }
 });
 
+
+// Images route
+app.use('/images', express.static('backend/scripts/db/images'));
+
 // Server start
 const PORT = process.env.PORT || 5000;
 
