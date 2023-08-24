1b16b2316db1898722f53cddb649c3e212c9da45
evanrich2404 <evanrich2404@gmail.com>
2023-08-23 19:59:56 -0500

    trying to get shopping page up

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+
+## Getting setup
+
+So to get this setup you need to have Vite loaded in a terminal, nodemon server loaded in a terminal, and a mysql server setup and with a functional db
--- frontend/src/pages/shopping.jsx
+++ frontend/src/pages/shopping.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+useEffect(() => {
+  async function fetchProductImages(product) {
+      try {
+          const response = await axios.get(product.product_image, { responseType: 'arraybuffer' });
+          const blob = new Blob([response.data], { type: 'image/png' });
+          const imageSrc = URL.createObjectURL(blob);
+          product.cachedImage = imageSrc;
+          return product;
+      } catch (error) {
+          console.error("There was an error fetching the image:", error);
+      }
+  }
+
+  products.forEach(fetchProductImages);
+
+}, [products]);
+
 
 export const Product = (props) => {
   // eslint-disable-next-line no-unused-vars
-  const { id, name, price, image } =  props.data;
+  const { product_name: name, product_price: price, product_description: description } =  props.data;
   return (
     <div className="product">
-      <img src={frontend/src/assets/product}/>
+      <img src={`/images/product17.png`} alt={`Image of ${name}`}/>
       <div className="description">
         <p>
-          <b>{name}</b>
+          <b>{name}</b><span>{description}</span>
         </p>
         <p> ${price}</p>
       </div>
@@ -18,15 +38,24 @@ export const Product = (props) => {
 
 
 const Shopping = () => {
-  const products = [
-    { id: 1, name: 'Product 1', price: 10, image: 'path_to_image1.jpg' },
-    { id: 2, name: 'Product 2', price: 15, image: 'path_to_image2.jpg' },
-    // ... Add as many products as you want
-  ];
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get('/api/products');
+        setProducts(response.data);
+      } catch (error) {
+        console.error("There was an error fetching the products:", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
 
   return (
     <div className="shopping-container">
-      {products.map(product => <Product key={product.id} data={product} />)}
+      {products.map(product => <Product key={product.product_id} data={product} />)}
     </div>
   );
 };
